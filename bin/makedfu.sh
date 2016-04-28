67c386176f696c75f7bb03012f8ed246efbda1cd
jaggies <jaggiesjim@gmail.com>
2016-04-27 23:43:09 -0700

    Simplify build script

--- bin/makedfu.sh
+++ bin/makedfu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #pip install intelhex
 #sudo pip install intelhex
-arm-none-eabi-objcopy -O ihex build/Users/jmiller/trees/open-source/gcc-arm-none-eabi-4_9-2015q3/bin/arm-none-eabi/main /tmp/main.hex
-python ~/trees/workspace/alice/alice3/io_board/stm32f4/dfu-convert -i /tmp/main.hex /tmp/main.dfu
-python ~/trees/workspace/alice/alice3/io_board/stm32f4/dfu-convert -i /tmp/main.hex /tmp/main.dfu
-dfu-util -d "0483:df11" -a0 -D /tmp/main.dfu
 #system_profiler SPUSBDataType
+arm-none-eabi-objcopy -O ihex build/arm-none-eabi/main /tmp/main.hex
+python bin/dfu-convert -i /tmp/main.hex /tmp/main.dfu
+dfu-util -d "0483:df11" -a0 -D /tmp/main.dfu

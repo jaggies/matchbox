b55ce89b9016ff6f94e7f1c33817c281a6300566
jaggies <jaggiesjim@gmail.com>
2016-07-05 19:36:45 -0700

    Add very simple "blink" application.

---
+++ apps/blink/blink.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stm32f4xx_hal.h>
+
+int main(int argc, char** argv)
+{
+    HAL_Init();
+}
---
+++ apps/blink/sys.c
@@ -0,0 +1,25 @@
+/*
+ * sys.c
+ *
+ *  Created on: Jul 5, 2016
+ *      Author: jmiller
+ */
+
+void _exit(int status)
+{
+    while (1);
+}
+
+void _kill(int pid)
+{
+
+}
+
+int _getpid()
+{
+    return 0;
+}
+
+
+
+

f2c0cfe5907f56b8e88e94fe1a47c8782a743050
jaggies <jaggiesjim@gmail.com>
2016-07-10 04:22:29 -0700

    Move devices into matchbox class - Make structures static members to workaround suspected malloc() bug.

--- lib/matchbox/matchbox.h
+++ lib/matchbox/matchbox.h
@@ -13,11 +13,25 @@ class MatchBox {
         MatchBox();
         ~MatchBox();
         SPI_HandleTypeDef* getSpi1() { return &hspi1; }
+        SPI_HandleTypeDef* getSpi2() { return &hspi2; }
+        ADC_HandleTypeDef* getAdc1() { return &hadc1; }
     private:
         void gpioInit(void);
         void systemClockConfig(void);
         void spi1Init(void);
-        SPI_HandleTypeDef hspi1;
+        void spi2Init(void);
+        void adc1Init(void);
+        void i2c1Init(void);
+        void usart1Init(void);
+        void usart2Init(void);
+
+        // TODO: make these local once malloc bug is fixed
+        static SPI_HandleTypeDef hspi1;
+        static SPI_HandleTypeDef hspi2;
+        static UART_HandleTypeDef huart1;
+        static UART_HandleTypeDef huart2;
+        static ADC_HandleTypeDef hadc1;
+        static I2C_HandleTypeDef hi2c1;
 };
 
 #endif /* MATCHBOX_H_ */

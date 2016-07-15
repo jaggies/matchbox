c0f217981fad1b2429c02f3cb0eb36877767afad
jaggies <jaggiesjim@gmail.com>
2016-07-15 03:15:20 -0700

    Add ADC abstraction layer.

--- lib/matchbox/matchbox.h
+++ lib/matchbox/matchbox.h
@@ -9,29 +9,21 @@
 #define MATCHBOX_H_
 
 #include "usbd_core.h"
+#include "board.h"
 
 class MatchBox {
     public:
         MatchBox();
         ~MatchBox();
-        SPI_HandleTypeDef* getSpi1() { return &hspi1; }
-        SPI_HandleTypeDef* getSpi2() { return &hspi2; }
-        ADC_HandleTypeDef* getAdc1() { return &hadc1; }
     private:
         void gpioInit(void);
         void systemClockConfig(void);
-        void spi1Init(void);
-        void spi2Init(void);
-        void adc1Init(void);
         void i2c1Init(void);
         void usart1Init(void);
         void usart2Init(void);
 
-        SPI_HandleTypeDef hspi1;
-        SPI_HandleTypeDef hspi2;
         UART_HandleTypeDef huart1;
         UART_HandleTypeDef huart2;
-        ADC_HandleTypeDef hadc1;
         I2C_HandleTypeDef hi2c1;
 };
 

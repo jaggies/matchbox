5acffda0618265b8cbe25e6d54b517097acf7ea1
jaggies <jaggiesjim@gmail.com>
2016-07-19 21:52:04 -0700

    Check for null button before calling handler.

--- lib/matchbox/button.cpp
+++ lib/matchbox/button.cpp
@@ -85,7 +85,7 @@ extern "C" void EXTI15_10_IRQHandler(void)
 
 extern "C" void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
     for (int i = 0; i < 16; i++) {
-        if ((1<<i) & GPIO_Pin) {
+        if (((1<<i) & GPIO_Pin) && Button::_buttons[i]) {
             Button::_buttons[i]->handleIrq();
         }
     }

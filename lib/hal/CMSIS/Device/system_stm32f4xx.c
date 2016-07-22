ca780f2bb084558348848d9684fe01387885abfa
jaggies <jaggiesjim@gmail.com>
2016-07-22 14:51:34 -0700

    Fix HSE_VALUE file to match actual crystal speed

--- lib/hal/CMSIS/Device/system_stm32f4xx.c
+++ lib/hal/CMSIS/Device/system_stm32f4xx.c
@@ -67,7 +67,8 @@
 #include "stm32f4xx.h"
 
 #if !defined  (HSE_VALUE) 
-  #define HSE_VALUE    ((uint32_t)25000000) /*!< Default value of the External oscillator in Hz */
+  #warning "HSE_VALUE not defined!"
+  #define HSE_VALUE    ((uint32_t)16000000) /*!< Default value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
 #if !defined  (HSI_VALUE)

b850c58cda773fb3c8e1df588e30bb7846e814a1
jaggies <jaggiesjim@gmail.com>
2016-07-21 23:16:29 -0700

    Add simple hello test case using new Pin class.

---
+++ apps/hello/hello.cpp
@@ -0,0 +1,37 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: Jun 20, 2016
+ *      Author: jmiller
+ */
+
+#include "matchbox.h"
+#include "pin.h"
+
+osThreadId defaultTaskHandle;
+void StartDefaultTask(void const * argument);
+
+int main(void) {
+    MatchBox* mb = new MatchBox();
+
+    osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 2048);
+    defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+    /* Start scheduler */
+    osKernelStart();
+
+    /* We should never get here as control is now taken by the scheduler */
+    while (1)
+        ;
+    return 0;
+}
+
+void StartDefaultTask(void const * argument) {
+    Pin led(LED_PIN, Pin::Config().setMode(Pin::MODE_OUTPUT));
+    int count = 0;
+    printf("Hello, world!\n");
+    while (1) {
+        led.write(count++ & 1);
+        osDelay(250);
+    }
+}
